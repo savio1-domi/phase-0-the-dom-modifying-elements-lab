6d1d9e9319c3bb15c6fbb57d43658db1e1892533
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-07 13:20:06 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write your code here!
-const main = document.getElementById("main")
-main.remove()
 
-const newHeader = document.createElement('h1')
-newHeader.setAttribute("id", "victory");
-newHeader.textContent = "Dominic is the champion "
+const testVar = "hi"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = " Dominic is the champion!";
+newHeader.className = "victory"; 
