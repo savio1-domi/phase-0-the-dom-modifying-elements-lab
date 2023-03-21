10ad432a0f4929a075ee4bc34dd59b39f56ab8a4
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-21 18:41:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ document.querySelector("main#main").remove();
 //Create your new element here and assign it to newHeader 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = " Dominic is the champion!";
+newHeader.innerHTML = " Dominic is the champion!";// my name
 newHeader.className = "victory"; 
