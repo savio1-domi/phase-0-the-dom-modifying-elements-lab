8bfc0ee8da3666a9f74d1dbc4000a47a1da54b3c
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-21 18:39:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const testVar = "hi"
 
-/* Enter the code to remove the node element here */
+// Enter the code to remove the node element here 
 document.querySelector("main#main").remove();
 
-/* Create your new element here and assign it to newHeader */
+//Create your new element here and assign it to newHeader 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerHTML = " Dominic is the champion!";
